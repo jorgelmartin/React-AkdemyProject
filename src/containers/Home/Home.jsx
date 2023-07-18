9b0f7ef0a4240b26ee87b3abf91ac5e175c691af
Jorge Martin <jorgemctin@gmail.com>
2023-07-18 19:55:58 +0200

    Feat: Home container

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+//SHOW SERVICES AT HOME PAGE
+export const Home = () => {
+    return (
+        <div className="homeDesign">
+            Hola Soy el Home
+            
+        </div>
+    );
+};   
