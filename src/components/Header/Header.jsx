c557a8cb20bcd5b06d773ec219ce4352eb1dca3c
Jorge Martin <jorgemctin@gmail.com>
2023-07-19 16:54:23 +0200

    Refactor: Header components with buttons Admin, Register, Login ...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,42 @@
 import React, { useEffect } from 'react';
 import "./Header.css";
-// import { useSelector, useDispatch } from "react-redux";
-// import { userData, logout } from "../../pages/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { userData, logout } from "../../containers/userSlice";
 import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
 
     //GETTING CREDENTIALS FROM REDUX
-    // const datosCredencialesRedux = useSelector(userData);
-    // const dispatch = useDispatch();
+    const datosCredencialesRedux = useSelector(userData);
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     
-    //
-    // useEffect(() => {
-    //     if (!datosCredencialesRedux.credentials?.token) {
-    //         navigate("/login");
-    //     }
-    // }, []);
-    //LOGOUT
-    // const handleLogout = () => {
-    //     dispatch(logout({ credentials: "" }));
-    //     navigate("/home");
-    // };
-
-
+    
+    useEffect(() => {
+        if (!datosCredencialesRedux.credentials?.token) {
+            navigate("/login");
+        }
+    }, []);
+    // LOGOUT
+    const handleLogout = () => {
+        dispatch(logout({ credentials: "" }));
+        navigate("/home");
+    };
 
     return (
         <div className='headerDesign'>
-            {/* <div className='linksDesign'>
+            <div className='linksDesign'>
                 <div className="headerLink" onClick={() => navigate("/home")}>:(SMILE:)</div>
             </div>
             {datosCredencialesRedux.data ? (
                 <div className="linksDesignToken">
-                    {datosCredencialesRedux.data.role === 3 ? (
+                    {datosCredencialesRedux.data.role === 2 ? (
                         <>
                             <div className="headerLink" onClick={() => navigate("/profile")}>Perfil</div>
                             <div className="headerLink" onClick={() => navigate("/appointment")}>Citas</div>
                             <div className="headerLink" onClick={handleLogout}>Salir</div>
                         </>
-                    ) : datosCredencialesRedux.data.role === 2 ? (
+                    ) : datosCredencialesRedux.data.role === 1 ? (
                         <>
                             <div className="headerLink" onClick={() => navigate("/profile")}>Perfil</div>
                             <div className="headerLink" onClick={() => navigate("/admin")}>Admin</div>
@@ -54,7 +52,7 @@ export const Header = () => {
             ) : (
                 <>
                 </>
-            )} */}
+            )}
         </div>
     );
 };
