3313cc04eef97ad41e7fc1f002dec3c5d164edb8
Jorge Martin <jorgemctin@gmail.com>
2023-07-22 02:49:55 +0200

    Fix: Login conection to Redux

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,67 +1,103 @@
 import React, { useState, useEffect } from "react";
 import "./Login.css";
-// import { InputText } from "../../common/InputText/InputText";
-// import { checkError, inputHandler } from "../../services/useful";
-// import { loginMe } from "../../services/apiCalls";
+
+import { loginMe } from "../../services/apiCalls";
 import { Button, Card, Col, Container, Row, Form } from "react-bootstrap";
-// import jwtDecode from "jwt-decode";
-// import { useNavigate } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
-// import { login, userData } from "../userSlice";
+import jwtDecode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login, userData } from "../userSlice";
+import { InputText } from "../../components/InputText/InputText";
 // import { inputHandler } from "../../services/useful";
 
 export const Login = () => {
 
-    // const [user, setUser] = useState({});
-    // const [token, setToken] = useState("");
-    // const [userError, setUserError] = useState({});
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
+    const [token, setToken] = useState("");
+    const [userError, setUserError] = useState({
+        emailError: false,
+        password: false
+    });
+
 
     //DISPATCH WRITE MODE
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
     //USESELECTOR READING MODE
     // const credentialsRdx = useSelector(userData);
 
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
     // const [welcome, setWelcome] = useState("");
 
-    // const [userLogin, setuserLogin] = useState({});
+    const [userLogin, setUserLogin] = useState({});
 
     //SENDING FORM LOGIN
+    const submitHandler = (e, body) => {
+        e.preventDefault();
+        console.log(body);
+        loginMe(body)
+            .then((res) => {
+                setToken(res.data.token);
+                setUserLogin(res.data.data);
+                console.log(res.data.token);
+            })
+            .catch((error) => {
+                setUserError({ credentials: error.response.data.message });
+            });
+    };
+    // USE DISPATCH 
+    useEffect(() => {
+        if (token) {
+            console.log("Soy el token", token);
+            console.log("Soy el tokendecoded", userLogin);
+            //No hace falta decodificar el token, llega deco del back
+            // let decoded = jwtDecode(token);
+            // console.log("Soy el tokendecoded", decoded);
+            dispatch(
+                login({
+                    token: token,
+                    name: userLogin.name, // Corregir esta línea
+                    role: userLogin.role_id,
+                    userId: userLogin.id,
+                })
+            );
+            navigate("/");
+        }
+    }, [token, userLogin]);
+
     // const submitHandler = (e, body) => {
     //     e.preventDefault();
     //     loginMe(body)
     //         .then((res) => {
-    //             setToken(res.data.token);
-    //             setuserLogin(res.data.user);
+    //             setToken(res.token);
+    //             console.log(res.data); // Verificar los detalles del objeto recibido en la consola
+    
+    //             // Despachar la acción de inicio de sesión con los datos del usuario obtenidos de la respuesta
+    //             dispatch(
+    //                 login({
+    //                     token: res.token,
+    //                     name: res.data.data.name,
+    //                     role: res.data.data.role,
+    //                     userId: res.data.data.id,
+    //                 })
+    //             );
+    
+    //             navigate("/");
     //         })
     //         .catch((error) => {
     //             setUserError({ credentials: error.response.data.message });
     //         });
     // };
-    //USE DISPATCH 
-    // useEffect(() => {
-    //     if (token) {
-    //         let decoded = jwtDecode(token);
-    //         dispatch(
-    //             login({
-    //                 token: token,
-    //                 name: userLogin.name,
-    //                 role: decoded.roleId,
-    //                 userId: decoded.userId
-    //             })
-    //         );
-    //         navigate("/");
-    //     }
-    // }, [token, userLogin]);
-
 
     return (
         <div className="loginDesign">
             {/* {welcome !== "" ? (
-        <div>{welcome}</div>
-      ) : ( */}
+            <div>{welcome}</div>
+        ) : ( */}
             {/* La utilidad de la siguiente linea es renderizar un hook at tiempo real en el DOM */}
             {/* {<pre>{JSON.stringify(credentials, null, 2)}</pre>} */}
             <Container className="d-flex justify-content-center align-items-center mt-4">
@@ -74,34 +110,34 @@ export const Login = () => {
                                     <Form.Group className="mb-3" controlId="formBasicEmail">
                                         <Form.Label>Email:</Form.Label>
                                         <Col>
-                                            {/* <InputText
+                                            <InputText
                                                 type={"email"}
-                                                design={userError.emailError ? "errorInput" : ""}
+                                                design={userError.emailError ? 'errorInput' : 'normalInput'}
                                                 name={"email"}
                                                 placeholder={"Enter email"}
                                                 state={setUser}
                                                 errorState={setUserError}
-                                            /> */}
+                                            />
                                         </Col>
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="formBasicPassword">
                                         <Form.Label>Password:</Form.Label>
                                         <Col>
-                                            {/* <InputText
+                                            <InputText
                                                 type={"password"}
-                                                design={userError.passwordError ? "errorInput" : ""}
+                                                design={userError.passwordError ? 'errorInput' : 'normalInput'}
                                                 name={"password"}
-                                                placeholder={"Password"}
+                                                placeholder={"Contraseña"}
                                                 state={setUser}
                                                 errorState={setUserError}
-                                            /> */}
+                                            />
                                         </Col>
                                     </Form.Group>
-                                    {/* {userError?.credentials ? (
+                                    {userError?.credentials ? (
                                         <div>{userError.credentials}</div>
                                     ) : (
                                         <div></div>
-                                    )} */}
+                                    )}
                                     <Button
                                         style={{ backgroundColor: '#13326fba' }}
                                         type="submit"
@@ -118,6 +154,5 @@ export const Login = () => {
                 </Card>
             </Container>
         </div>
-
     );
 };
