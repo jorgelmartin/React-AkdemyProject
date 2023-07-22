f20f0fcc958fd7ee3442093318fce474ae7f1776
Jorge Martin <jorgemctin@gmail.com>
2023-07-22 02:51:46 +0200

    Feat: UserSlice Redux

---
+++ src/containers/userSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        credentials: {
+            token: "",
+        },
+        data: {
+            name: "",
+            role: "",
+            userId: ""
+        },
+    },
+    reducers: {
+        login: (state, action) => {
+            let { payload } = action;
+            (state.credentials = {
+                token: payload.token,
+            }),
+                (state.data = {
+                    name: payload.name,
+                    role: payload.role,
+                    userId: payload.id
+                });
+        },
+        logout: (state) => {
+            return {
+                ...state,
+                credentials: {
+                    token: "",
+                },
+                data: {
+                    name: "",
+                    role: "",
+                },
+            }
+        }
+
+    }
+
+});
+
+//exporto las ACCIONES.....
+export const { login, logout } = userSlice.actions;
+
+export const userData = (state) => state.user;
+
+export default userSlice.reducer;
