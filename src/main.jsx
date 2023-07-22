1a34a591b4630669fb1e14094c9b35a7d1153303
Jorge Martin <jorgemctin@gmail.com>
2023-07-22 02:50:50 +0200

    Feat: persistor and provider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 //REDUX
-// import { Provider } from "react-redux";
-// import{ store, persistor} from "./redux/store.js"
+import { Provider } from "react-redux";
+import{ store, persistor} from "./redux/store.js"
 
 //REDUX PERSISTENCE....
-// import { PersistGate } from "redux-persist/integration/react";
-// import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 
 //ENVUELVE TODO PARA PODER INTERACTUAR
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+<React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
