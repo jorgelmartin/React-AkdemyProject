bba43999f25c5f720ee160c80c7c844021f9ed18
Jorge Martin <jorgemctin@gmail.com>
2023-07-22 17:52:34 +0200

    Feat: apiCall updateProfile to Profile

---
+++ src/containers/Profile/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Profile.css";
+import { ProfileCard } from "../../components/ProfileCard/ProfileCard";
+
+export const Profile = () => {
+    return (
+        <div className="userContainer">
+            <Container className="container-max-width">
+                <Row className="userCard gap-3">
+                    <Col>
+                        <div className="userCard">
+                            <ProfileCard></ProfileCard>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
