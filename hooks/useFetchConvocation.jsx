dfc76fbdc5075a446c9606d51010ea753689dd03
Jorge Martin <jorgemctin@gmail.com>
2023-07-24 21:52:14 +0200

    Feat: useFetchConvocation

---
+++ hooks/useFetchConvocation.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { userData } from "../src/containers/userSlice";
+
+export const useFetchAppointments = () => {
+    const [appointments, setAppointments] = useState(null);
+    const datosCredencialesRedux = useSelector(userData);
+
+    useEffect(() => {
+        let config = {
+            headers: {
+                Authorization: `Bearer ${datosCredencialesRedux.credentials?.token}`,
+            },
+        }
+        fetch('http://localhost:8000/api/convocation/getAll', config)
+            .then(res => res.json())
+            .then(res => {
+                setAppointments(res.data);
+            })
+            .catch(error => console.log("Error fetching appointments:", error))
+    }, []);
+
+    return appointments;
+};
