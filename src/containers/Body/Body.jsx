ffe9ebf3c02356481b9d04914ff26aed1e8d78c6
Jorge Martin <jorgemctin@gmail.com>
2023-07-26 18:17:18 +0200

    Refactor: naming convocation component

--- src/containers/Body/Body.jsx
+++ src/containers/Body/Body.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-// import { Home } from "../Home/Home";
+import { Home } from "../Home/Home";
 import { Login } from "../Login/Login";
+import { Register } from "../Register/Register";
+import { Profile } from "../Profile/Profile";
+import { Admin } from "../Admin/Admin";
+import { Users } from "../Users/Users";
+import { Convocation } from "../Convocation/Convocation";
+import { CreateConvocation } from "../CreateConvocation/CreateConvocation";
+
 
 export const Body = () => {
     return (
@@ -9,10 +16,14 @@ export const Body = () => {
             <Routes>
                 <Route path="*" element={<Navigate to="/"/>} />
                 <Route path="*" element={<Navigate to="/" />} />
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<CreateConvocation />} />
                 <Route path="/login" element={<Login />} />
-               {/*  <Route path="/register" element={<Register />} />
-                <Route path="/admin" element={<Admin />} /> */}
+                <Route path="/register" element={<Register />} /> 
+                <Route path="/profile" element={<Profile />} /> 
+                <Route path="/admin" element={<Admin />} /> 
+                <Route path="/users" element={<Users />} /> 
+                <Route path="/convocation" element={<Convocation />} /> 
+                <Route path="/createConvocation" element={<CreateConvocation />} /> 
             </Routes>
         </>
     );
--- src/containers/Convocation/Convocation.jsx
+++ src/containers/Convocation/Convocation.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useFetchAppointments } from "../../../hooks/useFetchConvocation";
-import { Col, Table, Container, Form, Card, Button } from "react-bootstrap";
+import { useFetchConvocations } from "../../../hooks/useFetchConvocation";
+import { Table, Container } from "react-bootstrap";
 import "./Convocation.css"
 import { useNavigate } from "react-router-dom";
 import { inputHandler } from "../../services/useful";
@@ -10,81 +10,81 @@ export const Convocation = () => {
     const [searchText, setSearchText] = useState("");
 
     //GET THE APPOINTMENT FROM THE HOOK
-    const appointments = useFetchAppointments();
+    const convocations = useFetchConvocations();
 
-    //STATE TO GET THE FILTERED APPOINTMENTS
-    const [filteredAppointments, setfilteredAppointments] = useState([]);
-    console.log(appointments);
+    //STATE TO GET THE FILTERED Convocations
+    const [filteredConvocations, setfilteredConvocations] = useState([]);
 
-    //UPDATE THE APPOINTMENTS WHEN CHANGE TH E STATE OF APPOINTMENTS
+    //UPDATE THE Convocations WHEN CHANGE TH E STATE OF Convocations
     useEffect(() => {
-        setfilteredAppointments(appointments)
-    }, [appointments])
+        setfilteredConvocations(convocations)
+    }, [convocations])
 
     //MAKE THE SEARCH FILTERED
     useEffect(() => {
         const searchAppointment = (text) => {
-            let filtered = appointments;
+            let filtered = convocations;
+            console.log(convocations);
 
-            if (text && appointments) {
-                filtered = (appointments.filter(
+            if (text && convocations) {
+                filtered = (convocations.filter(
                     (appointment) => appointment.dentist.name.includes(text) || appointment.patient.name.includes(text) || appointment.date.includes(text)))
             }
-            setfilteredAppointments(filtered);
+            setfilteredConvocations(filtered);
         }
         //ADD TIMEOUT
         const timeOutId = setTimeout(() =>
             searchAppointment(searchText.text), 500);
         return () => clearTimeout(timeOutId);
-    }, [searchText, appointments]);
+    }, [searchText, convocations]);
 
     const navigate = useNavigate();
-    if (!filteredAppointments) {
+    if (!filteredConvocations) {
         return <div>Cargando...</div>;
     }
-    console.log("soylaconvo", filteredAppointments);
     return (
         <div className="searchAppointment">
             <Container className="mt-5">
-    {/* INPUT SEARCH */}
-    <input
-        className="InputSearch"
-        type={"text"}
-        name={"text"}
-        placeholder={"Buscar cita..."}
-        onChange={(e) => inputHandler(e, setSearchText)}
-    />
-    {/* APPOINTMENT CARD */}
-    <Table striped bordered hover responsive>
-        <thead>
-            <tr>
-                <th>ID</th>
-                {/* Uncomment the line below to include patient details */}
-                {/* <th>Paciente</th> */}
-                <th>Programa</th>
-                <th>Inicio</th>
-                <th>Fin</th>
-            </tr>
-        </thead>
-        <tbody>
-            {filteredAppointments.map((convocation) => (
-                <tr key={convocation.id}>
-                    <td>{convocation.id}</td>
-                    {/* Uncomment the line below to include patient details */}
-                    {/* <td>{convocation.name} {convocation.user.surname}</td> */}
-                    <td>{convocation.program.name}</td>
-                    <td>{convocation.beginning}</td>
-                    <td>{convocation.end}</td>
-                    <div className="d-flex justify-content-center buttonsAppointments"> {/* Use d-flex and justify-content-between to display buttons side by side */}
-                <button className="buttonUpdate" onClick={() => navigate(`/appointmentdetail/${convocation.id}`)}>Detalle</button>
-                {/* <button className="buttonDelete">borrar</button> */}
-              </div>
-                </tr>
-            ))}
-        </tbody>
-        
-    </Table>
-</Container>
+                {/* INPUT SEARCH */}
+                <input
+                    className="InputSearch"
+                    type={"text"}
+                    name={"text"}
+                    placeholder={"Buscar cita..."}
+                    onChange={(e) => inputHandler(e, setSearchText)}
+                />
+                {/* APPOINTMENT CARD */}
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            {/* Uncomment the line below to include patient details */}
+                            {/* <th>Paciente</th> */}
+                            <th>Programa</th>
+                            <th>Inicio</th>
+                            <th>Fin</th>
+                            {/* <th>Descripción</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {filteredConvocations.map((convocation) => (
+                            <tr key={convocation.id}>
+                                <td>{convocation.id}</td>
+                                {/* Uncomment the line below to include patient details */}
+                                {/* <td>{convocation.name} {convocation.user.surname}</td> */}
+                                <td>{convocation.program.name}</td>
+                                <td>{convocation.beginning}</td>
+                                <td>{convocation.end}</td>
+                                {/* <td>{convocation.program.description}</td> */}
+                                <div className="d-flex justify-content-center buttonsConvocations"> {/* Use d-flex and justify-content-between to display buttons side by side */}
+                                    <button className="buttonUpdate" onClick={() => navigate(`/appointmentdetail/${convocation.id}`)}>Detalle</button>
+                                    {/* <button className="buttonDelete">borrar</button> */}
+                                </div>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Container>
         </div>
     );
 };
