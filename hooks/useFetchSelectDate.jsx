e43f34609f563f50e7e535616f09e243bb755ea7
Jorge Martin <jorgemctin@gmail.com>
2023-07-26 18:17:57 +0200

    Feat: useFetchSelectDate for create convocations

---
+++ hooks/useFetchSelectDate.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { isWeekend } from "date-fns";
+
+
+export const SelectDate = ({handleChange, value}) => {
+
+    //USER SELECTED DATE FROM THE FATHER COMPONENT
+    const [selectedDate, setSelectedDate] = useState(value ? new Date(value): null);
+
+    const handleDateChange = (date) => {
+        // VERIFY IF IS WEEKEND
+        if (!isWeekend(date)) {
+            setSelectedDate(date);
+            handleChange(date)
+        }
+    };
+
+    return (
+        <>
+            <DatePicker className="inputAppointment"
+                selected={selectedDate}
+                onChange={handleDateChange}
+                dateFormat="dd/MM/yyyy"
+                placeholderText="Selecciona una fecha"
+                showMonthDropdown
+                showYearDropdown
+                dropdownMode="select"
+                isClearable
+            />
+        </>
+    );
+};
