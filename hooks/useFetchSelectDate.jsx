6eeeaa5642b8a62d3c9576020a4be875b48dc6b0
Jorge Martin <jorgemctin@gmail.com>
2023-07-29 01:32:43 +0200

    Rename: classname input in fetch selector

--- hooks/useFetchSelectDate.jsx
+++ hooks/useFetchSelectDate.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { isWeekend } from "date-fns";
+import { isWeekend, format } from "date-fns";
 
-
-export const SelectDate = ({handleChange, value}) => {
-
+export const SelectDate = ({ handleChange, value }) => {
     //USER SELECTED DATE FROM THE FATHER COMPONENT
-    const [selectedDate, setSelectedDate] = useState(value ? new Date(value): null);
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    useEffect(() => {
+        // When the value from the parent changes, update the selectedDate state
+        setSelectedDate(value ? new Date(value) : null);
+    }, [value]);
 
     const handleDateChange = (date) => {
         // VERIFY IF IS WEEKEND
         if (!isWeekend(date)) {
             setSelectedDate(date);
-            handleChange(date)
+
+            // Format the date as "YYYY-MM-DD" before sending it to the parent component
+            const formattedDate = date ? format(date, "yyyy-MM-dd") : null;
+            handleChange(formattedDate);
         }
     };
 
     return (
         <>
-            <DatePicker className="inputAppointment"
+            <DatePicker
+                className="inputConvocation"
                 selected={selectedDate}
                 onChange={handleDateChange}
                 dateFormat="dd/MM/yyyy"
--- hooks/useFetchSelectSchedule.jsx
+++ hooks/useFetchSelectSchedule.jsx
@@ -1,28 +1,49 @@
-import { useEffect, useState } from "react";
+import React from "react";
 
 export const SelectSchedule = ({ handleChange, value }) => {
-
-    //USER SELECTED DOCTORS FROM THE FATHER COMPONENT
-    const [selectedSchedule, setSelectedSchedule] = useState(value);
-
     const schedules = [
         { "id": 1, "schedule": "Mañanas 09:00-14:00" },
-        { "id": 5, "schedule": "Tardes 15:00-20:00" }
+        { "id": 2, "schedule": "Tardes 15:00-20:00" }
     ];
 
     return (
-        <>
-            <select className="inputAppointment" value={selectedSchedule} onChange={(e) => {
-                handleChange(e.target.value);
-                setSelectedSchedule(e.target.value)
-            }}>
-                <option value="">Selecciona horario:</option>
-                {schedules.map((schedule) => (
-                    <option key={schedule.id} value={schedule.schedule}>
-                        {schedule.schedule}
-                    </option>
-                ))}
-            </select>
-        </>
+        <select className="inputConvocation" value={value} onChange={(e) => handleChange(e.target.value)}>
+            <option value="">Selecciona horario:</option>
+            {schedules.map((schedule) => (
+                <option key={schedule.id} value={schedule.schedule}>
+                    {schedule.schedule}
+                </option>
+            ))}
+        </select>
     );
 };
+
+
+// import { useEffect, useState } from "react";
+
+// export const SelectSchedule = ({ handleChange, value }) => {
+
+//     //USER SELECTED DOCTORS FROM THE FATHER COMPONENT
+//     const [selectedSchedule, setSelectedSchedule] = useState(value);
+
+//     const schedules = [
+//         { "id": 1, "schedule": "Mañanas 09:00-14:00" },
+//         { "id": 2, "schedule": "Tardes 15:00-20:00" }
+//     ];
+
+//     return (
+//         <>
+//             <select className="inputConvocation" value={selectedSchedule} onChange={(e) => {
+//                 handleChange(e.target.value);
+//                 setSelectedSchedule(e.target.value)
+//             }}>
+//                 <option value="">Selecciona horario:</option>
+//                 {schedules.map((schedule) => (
+//                     <option key={schedule.id} value={schedule.schedule}>
+//                         {schedule.schedule}
+//                     </option>
+//                 ))}
+//             </select>
+//         </>
+//     );
+// };
