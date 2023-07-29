4526757e51294c7c1ca29eb59c7d8c6d7d16c6c0
Jorge Martin <jorgemctin@gmail.com>
2023-07-30 00:34:57 +0200

    Feat: program image at Home page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,30 +3,32 @@ import { useFetchPrograms } from "../../../hooks/useFetchPrograms";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
-
 //SHOW PROGRAMS AT HOME PAGE
 export const Home = () => {
 
     const programs = useFetchPrograms();
 
     if (!programs) {
-        // Opción: Mostrar un mensaje de carga o un spinner mientras los datos se cargan.
         return <div>Cargando...</div>;
-      }
-    console.log();
+    }
+
     const navigate = useNavigate();
+    
     console.log("Soy los programas", programs);
+    
     return (
-        <>
-        {/* MAPPING SERVICES AT HOME PAGE */}
-            <div className="productCardContainer">
+        <div className="productCardContainer">
             {programs.map((program) => (
                 <div key={program.id} className="productCardDesign">
-                    {/* <img className="productImgDesign" src={program.image} alt={program.name} onClick={() => navigate(`/servicedetail/${product.id}`)} /> */}
+                    <img
+                        className="productImgDesign"
+                        src={`http://localhost:8000/${program.image}`}
+                        alt={program.name}
+                        onClick={() => navigate(`/programDetail/${program.id}`)}
+                    />
                     <h1>{program.name}</h1>
                 </div>
             ))}
         </div>
-        </>
     );
-};   
+};
