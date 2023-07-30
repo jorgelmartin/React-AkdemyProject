13874953d84ccd9c2ad747ece074ddd262e5523c
Jorge Martin <jorgemctin@gmail.com>
2023-07-30 18:40:07 +0200

    Update commits

--- src/services/ApiCalls.js
+++ src/services/ApiCalls.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = "http://localhost:8000";
+const URL = "https://laravel-akdemy-project.vercel.app";
 
 //LOGIN 
 export const loginMe = async (credentials) => {
