a8ac1a083949ace23109a673373fee7afc9bed56
Jorge Martin <jorgemctin@gmail.com>
2023-07-30 07:13:10 +0200

    Refactor: ProgramDetail handler

--- src/containers/ProgramDetail/ProgramDetail.jsx
+++ src/containers/ProgramDetail/ProgramDetail.jsx
@@ -1,30 +1,42 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./ProgramDetail.css";
 import { useFetchPrograms } from "../../../hooks/useFetchPrograms";
+import { useSelector } from "react-redux";
+import { userData } from "../userSlice";
 
-    export const ProgramDetail = () => {
-        //GET THE ID VALUE FROM THE URL
-        let { id } = useParams();
-        const parsedId = parseInt(id); 
+export const ProgramDetail = () => {
+    const datosCredencialesRedux = useSelector(userData);
+    const navigate = useNavigate();
+    //GET THE ID VALUE FROM THE URL
+    let { id } = useParams();
+    const parsedId = parseInt(id);
 
-        console.log("soyiddd", id);
-        //ASSIGN PROGRAMS
-        const programs = useFetchPrograms();
-        const [programDetail, setProgramDetail] = useState(null);
-    
-        useEffect(() => {
-            // FOUND THE PROGRAM WITH THE ID GET IT FROM THE ARRAY
-            const foundProduct = programs.find((item) => item.id === parsedId);
-            if (foundProduct) {
-                setProgramDetail(foundProduct);
-            } else {
-                setProgramDetail(null);
-            }
-        }, [parsedId, programs]);
-        console.log("soyiddd", programDetail);
+    console.log("soyiddd", id);
+    //ASSIGN PROGRAMS
+    const programs = useFetchPrograms();
+    const [programDetail, setProgramDetail] = useState(null);
 
-        //SHOW THE DETAIL SERVICE
+    useEffect(() => {
+        // FOUND THE PROGRAM WITH THE ID GET IT FROM THE ARRAY
+        const foundProduct = programs.find((item) => item.id === parsedId);
+        if (foundProduct) {
+            setProgramDetail(foundProduct);
+        } else {
+            setProgramDetail(null);
+        }
+    }, [parsedId, programs]);
+    console.log("soyiddd", programDetail);
+
+    const handleClick = () => {
+        if (!datosCredencialesRedux.credentials?.token) {
+            navigate('/register');
+        } else {
+            navigate('/inscription');
+        }
+    };
+
+    //SHOW THE DETAIL SERVICE
     return (
         <div className="DetailService">
             <div className="DetailService1">
@@ -32,11 +44,14 @@ import { useFetchPrograms } from "../../../hooks/useFetchPrograms";
                     <>
                         <h2>{programDetail.name}</h2>
                         <img src={`http://localhost:8000/${programDetail.image}`} alt="" />
-                        <h4>Descripción: {programDetail.description}</h4>
+                        <h4>{programDetail.description}</h4>
                         <h5>Precio €: {programDetail.price}</h5>
+                        <button className="SolicitarButton" onClick={handleClick}>
+                            Solicitar inscripción
+                        </button>
                     </>
                 ) : (
-                    <p>Producto no encontrado</p>
+                    <p>Cargando...</p>
                 )}
             </div>
         </div>
