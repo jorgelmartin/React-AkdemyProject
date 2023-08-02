5961b62d8e085996e32658c5decd438fe8e2f0ed
Jorge Martin <jorgemctin@gmail.com>
2023-08-02 16:02:31 +0200

    Refactor: convocationDetail

--- src/containers/ConvocationDetail/ConvocationDetail.jsx
+++ src/containers/ConvocationDetail/ConvocationDetail.jsx
@@ -4,6 +4,7 @@ import "./ConvocationDetail.css";
 import { Button } from "react-bootstrap";
 import { CreateConvocation } from "../CreateConvocation/CreateConvocation";
 import { useFetchConvocations } from "../../../hooks/useFetchConvocation";
+import { AkdemyButton } from "../../components/AkdemyButton/AkdemyButton";
 
 export const ConvocationDetail = () => {
 
@@ -32,31 +33,48 @@ export const ConvocationDetail = () => {
     console.log("convoooo",convocationDetail);
     return (
         <>
-            { convocationDetail && !editing ? (
-                <div className="convocationDetail">
-                    <div className="DetailAppointment1">
-                        <>
-                            <h2>Programa: {convocationDetail.program.name}</h2>
-                            <h4>Comienzo: {convocationDetail.beginning}</h4>
-                            <h5>Horarios: {convocationDetail.schedule}</h5>
-                            {/* <h5>Descripción: {convocationDetail.program.description}</h5>
-                            <h5>Precio: {convocationDetail.program.price}</h5> */}
-                        </>
-                        <Button style={{ backgroundColor: '#13326fba' }}
-                onClick={() => {
-                    setEditing(!editing);
-                }}
-            >
-                EDITAR
-            </Button>
+    {convocationDetail && !editing ? (
+        <div className="convocationDetail">
+            <div className="DetailAppointment1">
+                <div className="program-info">
+                    <h2>Programa: {convocationDetail.program.name}</h2>
+                    <div className="program-details">
+                        <div className="detail-item">
+                            <strong>Comienzo:</strong> {convocationDetail.beginning}
+                        </div>
+                        <div className="detail-item">
+                            <strong>Horarios:</strong> {convocationDetail.schedule}
+                        </div>
+                        <div className="detail-item">
+                            <strong>Descripción:</strong> {convocationDetail.program.description}
+                        </div>
+                        <div className="detail-item">
+                            <strong>Precio:</strong> {convocationDetail.program.price}
+                        </div>
                     </div>
                 </div>
-                
-                ) : (
-                <CreateConvocation isUpdate={true} updateData={convocationDetail}></CreateConvocation>
-                )
-            }
-            
-        </>
+                <AkdemyButton
+                    className="edit-button"
+                    onClick={() => {
+                        setEditing(!editing);
+                    }}
+                    text={"Editar"}
+                />
+            </div>
+            <h3>Alumnos:</h3>
+            <div className="user-list">
+                {convocationDetail.user.map((user) => (
+                    <div className="user-item" key={user.id}>
+                        <h4>{user.name} {user.surname}</h4>
+                        <p>Email: {user.email}</p>
+                        {/* Aquí puedes mostrar más información sobre el usuario si lo deseas */}
+                    </div>
+                ))}
+            </div>
+        </div>
+    ) : (
+        <CreateConvocation isUpdate={true} updateData={convocationDetail}></CreateConvocation>
+    )}
+</>
     );
 };
