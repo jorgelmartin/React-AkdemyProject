1475de5ca228b3ad31e186ed03e19d670802dcc2
Jorge Martin <jorgemctin@gmail.com>
2023-08-07 00:57:09 +0200

    Refactor: userRequest component

--- src/containers/userRequest/UserRequest.jsx
+++ src/containers/userRequest/UserRequest.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { useFetchUserRequest } from "../../../hooks/useFetchUserRequest";
 import { useFetchAcceptRequest } from "../../../hooks/useFetchAcceptRequest";
@@ -9,31 +9,35 @@ export const UserRequest = () => {
     const usersReq = useFetchUserRequest();
     const acceptUserRequest = useFetchAcceptRequest();
     const [requestAccepted, setRequestAccepted] = useState(false);
+    const [acceptedRequests, setAcceptedRequests] = useState([]);
 
-    if (!usersReq) {
-        return <div>Loading...</div>;
-    }
+//UPDATE THE COMPONENTE EACH CLIC
+    useEffect(() => {
+    }, [requestAccepted]);
 
     // ACCEPT REQUEST HANDLER
     const handleAcceptRequest = (id) => {
-        // e.preventDefault();
         acceptUserRequest(id)
             .then(() => {
                 console.log("Solicitud aceptada correctamente");
                 setRequestAccepted(true);
+
+                // FEAT THE ACCEPT REQUEST TO STATE acceptedRequests
+                setAcceptedRequests(prevRequests => [...prevRequests, id]);
             })
             .catch((error) => {
                 console.error("Error al aceptar la solicitud:", error);
             });
     };
 
+    if (!usersReq) {
+        return <div>Loading...</div>;
+    }
+    
+    
     return (
-
-        //RENDER USER REQUEST
         <Container className="mt-5">
-
             <div className="requestUser">Solicitudes pendientes</div>
-
             <div className="tableContainerCheck mt-4 tableScroll">
                 <div className="">
                     <div className="tableDataRow">
@@ -43,22 +47,27 @@ export const UserRequest = () => {
                         <div className="tableHeaderRequest"><strong>Inicio</strong></div>
                         <div className="tableHeaderRequest"><strong>Aceptar</strong></div>
                     </div>
-                    {usersReq.slice(0, 7).map((request, i) => (
-                        <div className="tableDataRow" key={i}>
-                            <div className="tableDataCheck">{request.user.name} {request.user.surname}</div>
-                            <div className="tableDataCheck">{request.user.email}</div>
-                            <div className="tableDataCheck">{request.program.name}</div>
-                            <div className="tableDataCheck">{request.convocation.beginning}</div>
-                            <div className="tableDataCheck">
-                                <AdminButton
-                                    onClick={() => {
-                                        handleAcceptRequest(request.id);
-                                    }}
-                                    text={"✔"}
-                                />
-                            </div>
-                        </div>
-                    ))}
+                    {usersReq.slice(0, 7).map((request, i) => {
+                        if (!acceptedRequests.includes(request.id)) {
+                            return (
+                                <div className="tableDataRow" key={i}>
+                                    <div className="tableDataCheck">{request.user.name} {request.user.surname}</div>
+                                    <div className="tableDataCheck">{request.user.email}</div>
+                                    <div className="tableDataCheck">{request.program.name}</div>
+                                    <div className="tableDataCheck">{request.convocation.beginning}</div>
+                                    <div className="tableDataCheck">
+                                        <AdminButton
+                                            onClick={() => {
+                                                handleAcceptRequest(request.id);
+                                            }}
+                                            text={"✔"}
+                                        />
+                                    </div>
+                                </div>
+                            );
+                        }
+                        return null; 
+                    })}
                 </div>
             </div>
         </Container>
