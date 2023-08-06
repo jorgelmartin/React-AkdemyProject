0bd810af4a916dbc62086cef8afd7fc615270514
Jorge Martin <jorgemctin@gmail.com>
2023-08-06 23:26:12 +0200

    Refactor: userRequest table

--- src/containers/userRequest/UserRequest.jsx
+++ src/containers/userRequest/UserRequest.jsx
@@ -4,6 +4,7 @@ import { useFetchUserRequest } from "../../../hooks/useFetchUserRequest";
 import { useFetchAcceptRequest } from "../../../hooks/useFetchAcceptRequest";
 import "../../App.css";
 import { AkdemyButton } from "../../components/AkdemyButton/AkdemyButton";
+import { AdminButton } from "../../components/AdminButton/AdminButton";
 
 export const UserRequest = () => {
     const usersReq = useFetchUserRequest();
@@ -32,33 +33,31 @@ export const UserRequest = () => {
         //RENDER USER REQUEST
         <Container className="mt-5">
 
-            {/* PENDING REQUEST TITLE */}
             <div className="requestUser">Solicitudes pendientes</div>
-            <div className="tableContainerCheck mt-4">
 
-                {/* TABLE PENDING REQUEST */}
-                <div className="tableHeader">
-                    <div className="tableHeaderCheck">Nombre</div>
-                    <div className="tableHeaderCheck">Email</div>
-                    <div className="tableHeaderCheck">Curso</div>
-                    <div className="tableHeaderCheck">Inicio</div>
-                </div>
-                <div className="tableBodyCheck">
-
-                    {/* MAPPING PENDING USER REQUEST */}
-                    {usersReq.slice(0, 7).map((request, index) => (
-                        <div className="tableDataRow" key={index}>
+            <div className="tableContainerCheck mt-4 tableScroll">
+                <div className="">
+                    <div className="tableDataRow">
+                        <div className="tableHeaderRequest"><strong>Nombre</strong></div>
+                        <div className="tableHeaderRequest"><strong>Email</strong></div>
+                        <div className="tableHeaderRequest"><strong>Curso</strong></div>
+                        <div className="tableHeaderRequest"><strong>Inicio</strong></div>
+                        <div className="tableHeaderRequest"><strong>Aceptar</strong></div>
+                    </div>
+                    {usersReq.slice(0, 7).map((request, i) => (
+                        <div className="tableDataRow" key={i}>
                             <div className="tableDataCheck">{request.user.name} {request.user.surname}</div>
                             <div className="tableDataCheck">{request.user.email}</div>
                             <div className="tableDataCheck">{request.program.name}</div>
-                            <div className="tableDataCheck">{request.convocation.beginning}
-                        </div>
-
-                            {/* AKDEMY BUTTON WITH THE HANDLER */}
-                            <AkdemyButton onClick={() => {
-                                handleAcceptRequest(request.id);
-                            }}
-                                text={"✔"} />
+                            <div className="tableDataCheck">{request.convocation.beginning}</div>
+                            <div className="tableDataCheck">
+                                <AdminButton
+                                    onClick={() => {
+                                        handleAcceptRequest(request.id);
+                                    }}
+                                    text={"✔"}
+                                />
+                            </div>
                         </div>
                     ))}
                 </div>
