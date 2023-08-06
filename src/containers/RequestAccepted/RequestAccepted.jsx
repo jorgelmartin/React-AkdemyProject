69f15572452ec31ae86b265e7d8c6c44bd8b4b90
Jorge Martin <jorgemctin@gmail.com>
2023-08-06 05:49:05 +0200

    Refactor: message inscription

--- src/containers/RequestAccepted/RequestAccepted.jsx
+++ src/containers/RequestAccepted/RequestAccepted.jsx
@@ -19,15 +19,15 @@ export const RequestAccepted = () => {
             <p>Nos complace informarle que hemos recibido su solicitud y estamos encantados de poder asistirle en su proceso de formación.</p>
             <p>Para proceder con el trámite, le solicitamos amablemente que nos haga llegar la siguiente documentación, ya sea de manera presencial o por correo electrónico a akdemyproject@outlook.com:</p>
             <ul>
-                <li>DNI/NIE (Por ambas caras).</li>
-                <li>Depósito bancario indicando nombre completo y curso a la siguiente cuenta: ES82 2333 3333 3333 3333 3333.</li>
+                <li>Documento de identidad (Por ambas caras).</li>
+                <li>Depósito bancario indicando nombre completo y curso a la siguiente cuenta: XXXX XXXX XXXX XXXX.</li>
             </ul>
             <p>Tenga en cuenta que podrá llevar un registro detallado de sus cursos en el apartado "Mis cursos" de nuestra plataforma.</p>
             <p>Si surgiera cualquier inquietud o si necesita asistencia adicional, no dude en ponerse en contacto con nosotros. Estamos a su disposición para brindarle toda la ayuda que requiera.</p>
             <p>Gracias por confiar en nosotros. Esperamos recibir su documentación pronto.</p>
             <p>Atentamente,</p>
             <p>El equipo de Akdemy</p>
-            <p>Teléfono de contacto: +34-911 111 111</p>
+            <p>Teléfono de contacto: +99999999</p>
 
             {/* AKDEMY BUTTON TO GO BACK */}
             <AkdemyButton text={"Volver"} onClick={handleBackClick}/>
