90e3055f645bd35520af8e69676a2697b880af72
Jorge Martin <jorgemctin@gmail.com>
2024-04-10 04:24:13 +0200

    Feat: PageButton component

---
+++ src/components/PageButton/PageButton.jsx
@@ -0,0 +1,22 @@
+import './PageButton.css';
+
+//CREATE COMPONENT AKDEMY BUTTON
+export const PageButton = ({ onClick, text, disabled, design }) => {
+
+    const buttonClasses = ["pageButton"];
+    if (design === "right") {
+        buttonClasses.push("pageRight");
+    } else if (design === "left") {
+        buttonClasses.push("pageLeft");
+    }
+
+    return (
+        <button
+            className={buttonClasses.join(" ")}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {text}
+        </button>
+    );
+};
